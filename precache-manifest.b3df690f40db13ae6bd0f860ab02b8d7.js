141a9ef7d044864502bff649779ae97dc86066dc
Matt Stanton <luchak@gmail.com>
2019-05-09 09:43:17 -0700

    Updates

---
+++ precache-manifest.b3df690f40db13ae6bd0f860ab02b8d7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fb75c054c22844b2b624f8033237614",
+    "url": "/freeform-vote/index.html"
+  },
+  {
+    "revision": "80d4d0efac3e6d12ad48",
+    "url": "/freeform-vote/static/css/main.f81a83d4.chunk.css"
+  },
+  {
+    "revision": "1e431ebb2752d5ee4f7a",
+    "url": "/freeform-vote/static/js/2.00fcf9cc.chunk.js"
+  },
+  {
+    "revision": "80d4d0efac3e6d12ad48",
+    "url": "/freeform-vote/static/js/main.78a07821.chunk.js"
+  },
+  {
+    "revision": "bdec7dc75a8fca69b746",
+    "url": "/freeform-vote/static/js/runtime~main.0318008a.js"
+  }
+]);
